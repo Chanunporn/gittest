767dfd0028de570f3a9a2de1e7908cdafe355e1d
Chanunporn <chanunporn.pongsri@mail.kmutt.ac.th>
2020-10-20 17:15:50 +0700

    new feature

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -16,10 +16,15 @@ public class Gittest {
      */
     public static void main(String[] args) {
         sayHello();
+        sayGoodbye();
 // TODO code application logic here
     }
     
     public static void sayHello(){
         System.out.println("Hello");
+    }
+    
+    public static void sayGoodbye(){
+        System.out.println("Goodbye");
     }
 }
